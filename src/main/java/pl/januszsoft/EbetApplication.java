b80ca3624051fe0987c1ec48dc8205b47432abd9
janek@projmen.pl <janek@projmen.pl>
2017-08-07 19:55:55 +0200

    init

---
+++ src/main/java/pl/januszsoft/EbetApplication.java
@@ -0,0 +1,12 @@
+package pl.januszsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbetApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/januszsoft/EbetApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.januszsoft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EbetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
