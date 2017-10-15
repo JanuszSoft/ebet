645a0691b252e5e1c20f1e66ac157bdef8fbcf0a
janek@projmen.pl <janek@projmen.pl>
2017-10-15 11:57:28 +0200

    Add NotYetImplementedException

---
+++ src/main/java/pl/januszsoft/error/NotYetImplementedException.java
@@ -0,0 +1,8 @@
+package pl.januszsoft.error;
+
+public class NotYetImplementedException extends RuntimeException {
+
+    public NotYetImplementedException(String message) {
+        super(message);
+    }
+}
